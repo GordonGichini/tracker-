08d6c5746d4a759b1983034009a71bebba3497b7
GordonGichini <gordongichini1@gmail.com>
2022-08-05 00:57:55 +0300

    editting context file

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,16 +1,23 @@
 // Reducer is a function that takes in the old state and an action and returns a new state
 
 const contextReducer = (state, action) => {
+    let transactions;
 switch (action.type){
     case 'DELETE_TRANSACTION':
-        const transactions = state.filter((t) => t.id ===)
+         transactions = state.filter((t) => t.id !== action.payload);
 
-    break;
-    case 'ADD_TRANSACTION':
-        break;
-        default:
-            break;
-}
-}
+         localStorage.setItem('transactions', JSON.stringify(transactions));
 
-export default contextReducer;
+         return transactions;
+       case 'ADD_TRANSACTION':
+         transactions = [action.payload, ...state];
+   
+         localStorage.setItem('transactions', JSON.stringify(transactions));
+   
+         return transactions;
+       default:
+         return state;
+     }
+   };
+   
+   export default contextReducer;
