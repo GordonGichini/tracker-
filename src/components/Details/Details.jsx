7b9affe69e73e0378dba509d78dd3dddb7528965
GordonGichini <gordongichini1@gmail.com>
2022-08-05 01:17:34 +0300

    modified files

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -3,19 +3,21 @@ import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
 
 import useStyles from './styles';
+import useTransactions from '../../useTransactions';
 
-const Details = ({ title }) => {
+const DetailsCard = ({ title, subHeader }) => {
+    const { total, chartData } = useTransactions(title);
     const classes = useStyles();
 
 return (
     <Card className={title === 'Income' ? classes.income : classes.expense}>
-        <CardHeader title={title} />
+        <CardHeader title={title} subHeader={subHeader} />
             <CardContent>
-                <Typography variant="h5">$50</Typography>
-                <Doughnut data="DATA" />
+                <Typography variant="h5">${total}</Typography>
+                <Doughnut data={chartData} />
             </CardContent>
     </Card>
-)
-}
+);
+};
 
-export default Details;
+export default DetailsCard;
--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -7,34 +7,29 @@ import useStyles from './styles';
 
 const List = () => {
     const classes = useStyles();
-    const { deleteTransaction } = useContext(ExpenseTrackerContext);
-    
-
-    const transactions = [
-        { id: 1, type: "Income", category: 'Salary', amount:50, date:"Wed Jan 26" },
-        { id: 2, type: "Expense", category: 'Pets', amount:50, date:"Thur Jan 27" },
-        { id: 3, type: "Income", category: 'Business', amount:250, date:"Fri Jan 28" }
-    ];
-
-    return(
-        <MUIList dense={false} className={classes.list}>
-            {transactions.map((transaction) => (
-                <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
-                    <ListItemAvatar>
-                        <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
-                            <MoneyOff />
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`}>
-                        <ListItemSecondaryAction>
-                            <IconButton edge="end" aria-label="delete" onClick="">
-                                <Delete />
-                            </IconButton>
-                        </ListItemSecondaryAction>
-                    </ListItemText>
-                </Slide>
-            ))}
-        </MUIList>
-
-    )
-}
+    const { transactions, deleteTransaction } = useContext(ExpenseTrackerContext);
+  
+    return (
+      <MUIList dense={false} className={classes.list}>
+        {transactions.map((transaction) => (
+          <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
+                  <MoneyOff />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
+              <ListItemSecondaryAction>
+                <IconButton edge="end" aria-label="delete" onClick={() => deleteTransaction(transaction.id)}>
+                  <Delete />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          </Slide>
+        ))}
+      </MUIList>
+    );
+  };
+  
+  export default List;
